eaaf03446110d934d3b6f99f3f926cc9609be528
h.o.f.522@hotmail.com <h.o.f.522@hotmail.com>
2019-02-02 20:55:53 +0300

    first commit

---
+++ app/src/main/java/eOccasion/com/Main2Activity.java
@@ -0,0 +1,14 @@
+package eOccasion.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.information_eventbtn);
+    }
+}
---
+++ app/src/main/java/eOccasion/com/accept.java
@@ -0,0 +1,14 @@
+package eOccasion.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class accept extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_offers);
+    }
+}
